22a61ed225a36215685a508be3493a3cd0993309
Brittany Storoz <brittanystoroz@gmail.com>
2014-05-07 14:06:12 -0400

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,9 +84,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-jsvalidate');
   grunt.loadNpmTasks('grunt-release');
 
-  grunt.registerTask('default', ['install-hook', 'jsbeautifier', 'mocha']);
+  grunt.registerTask('default', ['install-hook', 'jshint', 'jsvalidate', 'jsbeautifier', 'mocha']);
   grunt.registerTask('default', []);
-  grunt.registerTask('precommit', ['jsvalidate', 'jsbeautifier', 'mocha']);
+  grunt.registerTask('precommit', ['jsvalidate', 'jshint', 'jsbeautifier', 'mocha']);
 
   grunt.registerTask('install-hook', function() {
     var fs = require('fs');
